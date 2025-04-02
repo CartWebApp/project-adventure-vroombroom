0690fbfb577731dddcac7586c8b6d2a718db483f
Dev-AspiringVibrant <devin.silkeutsabay24@student.cart.org>
2025-04-02 10:42:55 -0700

    c

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import "./text.js";
+
 // Text history
 var historyButton = document.getElementById("history");
 var readText = document.getElementById("read");
@@ -13,18 +15,35 @@ closeReadText.onclick = function() {
 
 // Start
 var startButton = document.getElementById("start");
+var gameTitle = document.getElementById("title");
 
 startButton.addEventListener("click", transition)
 
 // Remove Start
 startButton.addEventListener("click", () => {
     startButton.style.display = "none";
+    continueButton.style.display = "block";
+    gameTitle.style.display = "none";
 })
 
 // Transition
-
 function transition() {
     var imageTransition = document.getElementById("image-transition");
 
     imageTransition.style.opacity = "100%";
 }
+
+function transitionNot() {
+    var imageTransition = document.getElementById("image-transition");
+
+    imageTransition.style.opacity = "0%";
+}
+
+// Text
+var dialogue = document.getElementById("dialogue");
+var options = document.getElementById("option-list");
+var continueButton = document.getElementById("continue");
+
+startButton.addEventListener("click", () => {
+    dialogue.innerHTML = partOne[0];
+})
---
+++ text.js
@@ -0,0 +1,5 @@
+// Part 1
+let partOne = [
+    "You wake up.",
+    "Part two",
+]
