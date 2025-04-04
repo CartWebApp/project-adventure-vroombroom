449c87562d08b0023e66463eb117c3b2487802db
Dev-AspiringVibrant <devin.silkeutsabay24@student.cart.org>
2025-04-04 10:42:56 -0700

    g

--- game.js
+++ game.js
@@ -19,7 +19,11 @@ closeReadText.onclick = function() {
 var startButton = document.getElementById("start");
 var gameTitle = document.getElementById("title");
 
-startButton.addEventListener("click", transition)
+startButton.addEventListener("click", async function () {
+    await transition()
+    await wait(1000);
+    transitionNot();
+})
 
 // Remove Start
 
@@ -33,13 +37,15 @@ startButton.addEventListener("click", () => {
 
 function transition() {
     var imageTransition = document.getElementById("image-transition");
-
     imageTransition.style.opacity = "100%";
+}
+
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function transitionNot() {
     var imageTransition = document.getElementById("image-transition");
-
     imageTransition.style.opacity = "0%";
 }
 
@@ -52,10 +58,13 @@ var currentDialogue = 1;
 var storyProgress = 0;
 var storySoFar = document.getElementById("historical-text");
 
-startButton.addEventListener("click", () => {
-    dialogue.innerHTML = dialoguePart[0][0];
-    storySoFar.innerHTML = dialoguePart[0][0];
-})
+startButton.addEventListener("click", async function () {
+    await wait(1000);
+    typeText(dialogue, dialoguePartText[0][0], 10, () => {
+        storySoFar.innerHTML = dialoguePartText[0][0];
+    });
+});
+
 
 // Continue
 
@@ -64,17 +73,59 @@ continueButton.addEventListener("click", playDialogue)
 // Progression
 
 function playDialogue() {
-    if (currentDialogue < dialoguePart[storyProgress].length) {
-        dialogue.innerHTML = dialoguePart[storyProgress][currentDialogue];
-        storySoFar.innerHTML += "<br/><br/>" + dialoguePart[storyProgress][currentDialogue];
-        currentDialogue += 1;
+    if (isTyping) return; // Prevent overlapping calls
+    isTyping = true;
+
+    if (currentDialogue < dialoguePartText[storyProgress].length) {
+        typeText(dialogue, dialoguePartText[storyProgress][currentDialogue], 10, () => {
+            storySoFar.innerHTML += "<br/><br/>" + dialoguePartText[storyProgress][currentDialogue];
+            currentDialogue += 1;
+            isTyping = false; // Reset flag when finished
+        });
     } else {
         storyProgress += 1;
         currentDialogue = 0;
-        if (dialoguePart[storyProgress]) {
-            dialogue.innerHTML = dialoguePart[storyProgress][currentDialogue];
-            storySoFar.innerHTML += "<br/><br/>" + dialoguePart[storyProgress][currentDialogue];
-            currentDialogue += 1;
+        if (dialoguePartText[storyProgress]) {
+            typeText(dialogue, dialoguePartText[storyProgress][currentDialogue], 10, () => {
+                storySoFar.innerHTML += "<br/><br/>" + dialoguePartText[storyProgress][currentDialogue];
+                currentDialogue += 1;
+                updateScene();
+                isTyping = false; // Reset flag when finished
+            });
         }
+    }
+}
+
+// Typing animation
+
+function typeText(element, text, speed, callback) {
+    let i = 0;
+    element.innerHTML = ""; // Clear previous text
+    let typingInterval = setInterval(() => {
+        element.innerHTML += text.charAt(i);
+        i++;
+        if (i >= text.length) {
+            clearInterval(typingInterval); // Stop typing
+            if (callback) callback(); // Execute the callback
+        }
+    }, speed);
+}
+
+let isTyping = false;
+
+// Picture
+
+function changePicture(storyPart, imageName) {
+  let thePicture = document.getElementById("image");
+  if (dialoguePartPicture[storyPart][imageName]) {
+    thePicture.src = dialoguePartPicture[storyPart][imageName];
+  } else {
+    console.log("Scene not found");
+  }
+}
+
+function updateScene() {
+    if (dialoguePartText[1][1]) {
+        changePicture(0, 1)
     }
 }
--- text.js
+++ text.js
@@ -1,6 +1,6 @@
 // Part one
 
-let partOne = [
+let partOneText = [
     "The morning sun had risen over the hills by the time you had finished practicing the forms with your brother's staff.",
     "It had been a year since he was dragged off to war without even a shoe to his name.",
     "You still think of it as his, holding the staff up to the light.",
@@ -12,9 +12,14 @@ let partOne = [
     "Over the hill, knights - foreign knights - race across the hillside, gunning towards the village, the village you call home.",
 ]
 
+let partOnePicture = [
+    "/img/grass.jpg",
+    "/img/staffimg.jpg",
+]
+
 // Part two
 
-let partTwo = [
+let partTwoText = [
     "Yes",
     "No",
     "test",
@@ -22,15 +27,16 @@ let partTwo = [
 
 // Part three
 
-let partThree = [
+let partThreeText = [
     "three",
     "three and three",
     "three, and three, and three",
 ]
 
-let partFour = [
+let partFourText = [
     "four",
     "second four",
 ]
 
-let dialoguePart = [partOne, partTwo, partThree, partFour]
+let dialoguePartText = [partOneText, partTwoText, partThreeText, partFourText]
+let dialoguePartPicture = [partOnePicture]
